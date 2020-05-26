f54a021ba0377dad93fe2835e5b21ade46417c63
Juan <juan@pop-os.localdomain>
2020-05-26 13:44:53 -0400

    created context

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import  Home from './components/pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home/>
     </div>
   );
 }
---
+++ portfolio-site/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+
+           <h4>This is Home!</h4>
+            
+        </div>
+    )
+}
+
+export default Home
