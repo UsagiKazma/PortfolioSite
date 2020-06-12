3dda849b31e45e72086728751dcd4324dd971dae
Juan <juan@pop-os.localdomain>
2020-06-11 20:13:12 -0400

    Done styling

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -14,25 +14,37 @@ const HomeIndex = () => {
       <Helmet>
         <title>{siteTitle}</title>
         <meta name="description" content={siteDescription} />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
+          rel="stylesheet"
+        ></link>
       </Helmet>
 
       <div id="main">
-        
         <section id="one">
           <h1>About Me</h1>
           <a href="#" className="image avatar">
-          <img src="https://res.cloudinary.com/dbrdhogvw/image/upload/v1584213627/Screen_Shot_2020-03-14_at_3.19.12_PM_ozq0ur.png"/>
+            <img src="https://res.cloudinary.com/dbrdhogvw/image/upload/v1584213627/Screen_Shot_2020-03-14_at_3.19.12_PM_ozq0ur.png" />
           </a>
-          <p>Originally from Queens, NY, now located in Miami, FL. I tend to approach situations from an analytical perspective. Currently I'm attending the General Assembly Software Engineering Immersive course and I'm looking to continue to sharpen my skills after graduation. I approach each situation and enjoy the satisfaction of learning something if for nothing else than to improve past projects. I look forward to hearing from you!</p>
+          <p>
+            Originally from Queens, NY, now located in Miami, FL. I tend to
+            approach situations from an analytical perspective. Currently I'm
+            attending the General Assembly Software Engineering Immersive course
+            and I'm looking to continue to sharpen my skills after graduation. I
+            approach each situation and enjoy the satisfaction of learning
+            something if for nothing else than to improve past projects. I look
+            forward to hearing from you!
+          </p>
         </section>
 
         <section id="two">
           <h1>Projects</h1>
 
           <Gallery />
-          
         </section>
 
         <section id="three">
